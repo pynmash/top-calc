1c0d11330b0900a51df76b04e865f658456ded61
Dan Toms <157254317+pynmash@users.noreply.github.com>
2026-01-22 15:54:30 +0000

    Define functions for basic calculation

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, num2, operator) {
+  return operator(num1, num2);
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
